f38e82bd8c263cd487ec72f9e1502acc847c0203
JoCh <johancz@users.noreply.github.com>
2021-05-05 22:26:30 +0200

    Add barebones SpaceportRepository and ISpaceportRepository

---
+++ Source/SpaceParkAPI/Repositories/ISpaceportsRepository.cs
@@ -0,0 +1,13 @@
+﻿using SpaceParkAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceParkAPI.Repositories
+{
+    public interface ISpaceportsRepository
+    {
+        Task<IEnumerable<Spaceport>> GetAllSpaceports(SpaceParkContext context);
+    }
+}
