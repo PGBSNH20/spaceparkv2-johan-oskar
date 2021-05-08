635385ed03a7469a5b2910b6c5997e05b6ea3aba
Oskar Karlsson <tilloskar@outlook.com>
2021-05-08 22:09:26 +0200

    Added migration

--- Source/SpaceParkAPI/Migrations/20210508200349_added_max_starshiplength_to_spaceportentity.Designer.cs
+++ Source/SpaceParkAPI/Migrations/20210508200349_added_max_starshiplength_to_spaceportentity.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using SpaceParkAPI.Models;
 
 namespace SpaceParkAPI.Migrations
 {
     [DbContext(typeof(SpaceParkContext))]
-    partial class SpaceParkContextModelSnapshot : ModelSnapshot
+    [Migration("20210508200349_added_max_starshiplength_to_spaceportentity")]
+    partial class added_max_starshiplength_to_spaceportentity
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Source/SpaceParkAPI/Migrations/20210508200349_added_max_starshiplength_to_spaceportentity.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpaceParkAPI.Migrations
+{
+    public partial class added_max_starshiplength_to_spaceportentity : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "MaxStarshipLength",
+                table: "Spaceports",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MaxStarshipLength",
+                table: "Spaceports");
+        }
+    }
+}
