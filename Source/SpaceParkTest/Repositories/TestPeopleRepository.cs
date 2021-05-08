f9ecb8ad8d16fecaabd53808d70bcb6e85e6f7a8
Oskar Karlsson <tilloskar@outlook.com>
2021-05-08 20:02:57 +0200

    Added reposotory to PeopleController and started working on TestPeopleRepo

---
+++ Source/SpaceParkTest/Repositories/TestPeopleRepository.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SpaceParkAPI.swapi.Repositories;
+using SpaceParkAPI.SWAPI.Models;
+
+namespace SpaceParkTest.Repositories
+{
+    class TestPeopleRepository : IPeopleRepository
+    {
+        public List<Person> People { get; set; }
+
+        public TestPeopleRepository(List<Person> people)
+        {
+            People = people ?? new List<Person>();
+        }
+
+        public Task<List<Person>> GetAllPersons()
+        {
+            return Task.FromResult(People);
+        }
+
+        public Task<ActionResult<Person>> GetPerson(string name)
+        {
+            var person = Task.FromResult(People.SingleOrDefault(p => p.Name == name));
+            return person;
+        }
+    }
+}
